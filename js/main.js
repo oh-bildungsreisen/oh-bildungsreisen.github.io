297a96aac4948a1d552ede03832c104f94f3cb1d
katjaq <katjaqheuer@gmail.com>
2019-01-03 23:41:25 +0100

    implemented menu bar to hide when entry clicked

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         },1000)
     })
 
+    /* for the image gallery */
     $(".fancybox").fancybox({
         openEffect: "none",
         closeEffect: "none"
@@ -20,9 +21,29 @@ $(document).ready(function(){
     });
 })
 
+/* go back to main page from single reise page */
 function goTo(url){
     window.location.href = url;
 }
+
+/* hide menu bar once an entry is clicked */
+$(function() {
+    var menuVisible = false;
+    $('#myMenuBtn').click(function() {
+        if (menuVisible) {
+            $('#myMenu').css({'display':'none'});
+            menuVisible = false;
+            return;
+        }
+        $('#myMenu').css({'display':'block'});
+        menuVisible = true;
+    });
+    $('#myMenu').click(function() {
+        $(this).css({'display':'none'});
+        menuVisible = false;
+    });
+});
+
 /*
 function formSubmitted() {
     $("form").html("Vielen Dank für Ihre Nachricht.");
